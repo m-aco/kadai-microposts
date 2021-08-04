6e0465c4a62d4d0c3ccecf01981c1bbaa279ee73
m-aco <m-aco02105@gmail.com>
2021-08-04 13:25:41 +0000

    top page

---
+++ app/controllers/toppages_controller.rb
@@ -0,0 +1,20 @@
+class ToppagesController < ApplicationController
+  def index
+  end
+  
+  def new
+    @toppage = Toppage.new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
